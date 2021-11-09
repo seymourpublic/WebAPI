ea723f1e005ff2bb0728c7198e706980a3777982
seymourpublic <naledisandamela@gmail.com>
2021-11-09 19:06:36 +0200

    Add project files.

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"Select Username, Email, Password From dbo.Users";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Project2Con");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+
+                myCon.Open();
+                using (SqlCommand myCom = new SqlCommand(query, myCon))
+                {
+                    myReader = myCom.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+
+
+                }
+
+
+            }
+            return new JsonResult(table);
+
+
+
+        }
+
+  //      [HttpPost]
+
+    }
+}
---
+++ WebAPI/Models/Albums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Albums
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string picture_name { get; set; }
+
+        public string album_name { get; set; }
+    }
+}
